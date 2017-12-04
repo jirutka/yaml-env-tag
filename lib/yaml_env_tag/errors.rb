fd39163fd33cefc1bb8bcfaa2cf32ec062f79aae
Jakub Jirutka <jakub@jirutka.cz>
2017-12-04 21:29:58 +0100

    Add attribute "variable_name" to MissingEnvVariableError

--- lib/yaml_env_tag/errors.rb
+++ lib/yaml_env_tag/errors.rb
@@ -5,8 +5,11 @@ module YamlEnvTag
   # is not set.
   class MissingEnvVariableError < ::RuntimeError
 
+    attr_reader :variable_name
+
     # @param variable_name [String] name of the environment variable.
     def initialize(variable_name)
+      @variable_name = variable_name
       super("Environment variable '#{variable_name}' is not set")
     end
   end
